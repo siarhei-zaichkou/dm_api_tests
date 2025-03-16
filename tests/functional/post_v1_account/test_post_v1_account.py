36d3af9ca670b55623a9b4e9082fd4c534616c79
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2025-03-16 21:11:06 +0300

    добавлена джоба для запуска тестов

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -2,15 +2,16 @@ from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 from json import loads
+from faker import Faker
 
 
 def test_post_v1_account():
-    # Регистрация пользователя
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'supermegatestlogin3'
+    # Регистрация пользователя
+    login = Faker().user_name()
     password = '123456'
     email = f'{login}@gmail.com'
     json_data = {
@@ -27,7 +28,6 @@ def test_post_v1_account():
 
     # Получение активационного токена
     token = get_activation_token_by_login(login, response)
-    print(token)
 
     # Активация пользователя
     response = account_api.put_v1_account_token(token)
@@ -41,7 +41,6 @@ def test_post_v1_account():
     }
     response = login_api.post_v1_account_login(json_data=json_data)
     assert response.status_code == 200, "Пользователь не смог авторизоваться"
-    print(response.text)
 
 
 def get_activation_token_by_login(login, response):
